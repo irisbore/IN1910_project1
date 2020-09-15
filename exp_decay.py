8ca6f804561ff80b3d4d00896a90b6a6388765dc
Silje Marie Bentdal <siljeben@uio.no>
2020-09-15 12:24:41 +0200

    Added method solve

--- exp_decay.py
+++ exp_decay.py
@@ -1,13 +1,36 @@
-import scipy
+import numpy as np
+from scipy.integrate import solve_ivp
 
 
 class ExponentialDecay:
+    """
+    Class that represents the exponential decay ODE
+
+    ...
+
+    Attributes
+    ----------
+    a : float
+        decay constant
+
+    Methods
+    -------
+    solve
+    """
+
     def __init__(self, a):
         self.a = a
 
     def __call__(self, t, u):
         return -self.a * u
 
+    def solve(self, u0, T, dt):
+        sol = solve_ivp(self.__call__, (0, T), (u0,))
+        return sol.t, sol.y[0]
+
 
 if __name__ == '__main__':
-    u = ExponentialDecay(0.4)
+    a = 0.4
+    u0 = [1, 2, 3, 4]
+    T = 5
+    dt = 0.1
--- test_exp_decay.py
+++ test_exp_decay.py
@@ -5,3 +5,9 @@ import exp_decay
 
 def test_call():
     assert exp_decay.ExponentialDecay(0.4)(0, 3.2) == pytest.approx(-1.28)
+
+
+def test_solve():
+    a, u0, T, dt = 0.4, 1, 7, 0.5
+    decay_model = exp_decay.ExponentialDecay(a)
+    t, u = decay_model.solve(u0, T, dt)
