e0368e3d41f91a0632ee9e5c0a0f52d7e6c0c9fa
Silje Marie Bentdal <siljeben@uio.no>
2020-09-20 17:19:16 +0200

    Added __call__ to DoublePendulum

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,40 +1,54 @@
+import numpy as np
+
 from pendulum import Pendulum
 
 
 class DoublePendulum:
-    g = 9.81
 
-    def __init__(self, M1=1, M2=1, L1=1, L2=1):
-        self.pendulum1 = Pendulum(L1, M1)
-        self.pendulum2 = Pendulum(L2, M2)
+    def __init__(self, M1=1, M2=1, L1=1, L2=1, G=9.81):
+        self.M1, self.M2, self.L1, self.L2, self.G = M1, M2, L1, L2, G
 
-    def delta(theta1, theta2):
+    def _delta(self, theta1, theta2):
         return theta2 - theta1
 
-    def domega1_dt(M1, M2, L1, L2, theta1, theta2, omega1, omega2):
+    def _domega1_dt(self, theta1, theta2, omega1, omega2):
         num = (
-            M2
-            * L1
+            self.M2
+            * self.L1
             * (omega1) ** 2
-            * np.sin(self.delta(theta1, theta2))
-            * np.cos(self.delta(theta1, theta2))
-            + M2 * G * np.sin(theta2) * np.cos(self.delta(theta1, theta2))
-            + M2 * L2 * (omega2) ** 2 * np.sin(self.delta(theta1, theta2))
-            - (M1 + M2) * G * np.sin(theta1)
+            * np.sin(self._delta(theta1, theta2))
+            * np.cos(self._delta(theta1, theta2))
+            + self.M2 * self.G * np.sin(theta2) *
+            np.cos(self._delta(theta1, theta2))
+            + self.M2 * self.L2 * (omega2) ** 2 *
+            np.sin(self._delta(theta1, theta2))
+            - (self.M1 + self.M2) * self.G * np.sin(theta1)
         )
-        den = (M1 + M2) * L1 - M2 * L1 * (np.cos(delta(theta1, theta2))) ** 2
+        den = (self.M1 + self.M2) * self.L1 - self.M2 * \
+            self.L1 * (np.cos(self._delta(theta1, theta2))) ** 2
         return num / den
 
-    def domega2_dt(M1, M2, L1, L2, theta1, theta2, omega1, omega2):
+    def _domega2_dt(self, theta1, theta2, omega1, omega2):
         num = (
-            -M2
-            * L2
+            -self.M2
+            * self.L2
             * omega2 ** 2
-            * np.sin(self.delta(theta1, theta2))
-            * np.cos(self.delta(theta1, theta2))
-            + (M1 + M2) * G * np.sin(theta1) * np.cos(self.delta(theta1, theta2))
-            - (M1 + M2) * L1 * omega1 ** 2 * np.sin(self.delta(theta1, theta2))
-            - (M1 + M2) * G * np.sin(theta2)
+            * np.sin(self._delta(theta1, theta2))
+            * np.cos(self._delta(theta1, theta2))
+            + (self.M1 + self.M2) * self.G * np.sin(theta1) *
+            np.cos(self._delta(theta1, theta2))
+            - (self.M1 + self.M2) * self.L1 * omega1 ** 2 *
+            np.sin(self._delta(theta1, theta2))
+            - (self.M1 + self.M2) * self.G * np.sin(theta2)
         )
-        den = (M1 + M2) * L2 - M2 * L2 * (np.cos(delta(theta1, theta2))) ** 2
+        den = (self.M1 + self.M2) * self.L2 - self.M2 * \
+            self.L2 * (np.cos(self._delta(theta1, theta2))) ** 2
         return num / den
+
+    def __call__(self, t, y):
+        return y[1], self._domega1_dt(y[0], y[2], y[1], y[3]), y[3], self._domega2_dt(y[0], y[2], y[1], y[3])
+
+
+if __name__ == '__main__':
+    dp = DoublePendulum()
+    dp(0, [np.pi / 6, 2, np.pi / 6, 1])
